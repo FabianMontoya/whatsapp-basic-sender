735ae681792b49019d87a9fb735148462d1ed70f
Fabian Dario Montoya <93332120+gt-fmontoya@users.noreply.github.com>
2022-06-12 00:54:25 -0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import { createI18n } from 'vue-i18n';
+
+import App from './App.vue';
+import router from './router';
+import languages from './i18n';
+
+const i18n = createI18n({
+  locale: 'es', // set locale
+  fallbackLocale: 'en', // set fallback locale
+  messages: languages // set locale messages
+});
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(i18n);
+
+app.mount('#app');
---
+++ src/stores/counter.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    counter: 0
+  }),
+  getters: {
+    doubleCount: (state) => state.counter * 2
+  },
+  actions: {
+    increment() {
+      this.counter += 1;
+    }
+  }
+});
